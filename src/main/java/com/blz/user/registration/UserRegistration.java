2cdc79387e33ac8159c0c440082ef8aa528df229
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-10 22:05:55 +0530

    Added: First name test case logic

--- src/main/java/com/blz/user/registration/UserRegistration.java
+++ src/main/java/com/blz/user/registration/UserRegistration.java
@@ -1,5 +1,23 @@
 package com.blz.user.registration;
 
-public class UserRegistration {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+	/*
+	 * @purpose: As a User need to enter a valid First Name. First name starts with
+	 * Cap and has minimum 3 characters
+	 * 
+	 * @param: firstName
+	 * 
+	 * @function: To check first name is Valid or not
+	 * 
+	 * @returns true or false
+	 */
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
 }
--- src/test/java/com/blz/user/registration/UserRegistrationTest.java
+++ src/test/java/com/blz/user/registration/UserRegistrationTest.java
@@ -1,5 +1,20 @@
 package com.blz.user.registration;
 
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class UserRegistrationTest {
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Rutuja");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("rutuja");
+		Assert.assertEquals(false, result);
+	}
 }
